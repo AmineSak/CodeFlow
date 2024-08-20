871cd605f99d5126ec1a7318213629b15278c8ee
Amine Sakouhi <sakouhiamine04@gmail.com>
2024-08-20 11:31:06 +0100

    multiple accounts for same email

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -31,7 +31,7 @@ const authOptions = {
         await connectToDB();
 
         // check if user already exists
-        const userExists = await User.findOne({ email: profile.email });
+        const userExists = await User.findOne({ email: profile.username });
 
         // if not, create a new document and save user in MongoDB
         if (!userExists) {
